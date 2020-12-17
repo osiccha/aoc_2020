c69157cadb73aaf8956348f0d96d602c8dfc9d57
osiccha <oliversiccha@gmx.de>
2020-12-17 20:02:45 +0100

    day10, part 2

--- day10/src/day10/core.clj
+++ day10/src/day10/core.clj
@@ -15,6 +15,15 @@
                  (#(string/split % #"\n")))]
     (map #(Integer/parseInt %) data)))
 
+(defn solve-subproblem
+  [adapter known-solutions]
+  (reduce #(+ %1
+              (if (nil? (get known-solutions %2))
+                0
+                (get known-solutions %2)))
+          0
+          (range (+ 1 adapter) (+ adapter 4))))
+
 (defn part1
   [data]
   (loop [prev 0
@@ -29,13 +38,27 @@
                2 values
                3 [ (first values) (+ 1 (second values))])))))
 
-      (defn -main
-        "AOC Day 10 entrypoint"
-        [& args]
-        (if (empty? args)
-          (println "Expected a path to the input file")
-          (->> args
-               first
-               get-data
-               part1
-               println)))
+(defn part2
+  [data]
+  (let [entries (sort >= (conj data 0))
+        highest-jolt (apply max entries)]
+    (loop [adapters entries
+           solutions {(+ 3 highest-jolt) 1}]
+      (if (= 0 (count  adapters))
+        (get solutions 0)
+        (recur (rest adapters)
+               (assoc solutions 
+                      (first adapters) 
+                      (solve-subproblem (first adapters) solutions)))))))
+
+(defn -main
+  "AOC Day 10 entrypoint"
+  [& args]
+  (if (empty? args)
+    (println "Expected a path to the input file")
+    (->> args
+         first
+         get-data
+         ;;part1
+         part2
+         println)))
